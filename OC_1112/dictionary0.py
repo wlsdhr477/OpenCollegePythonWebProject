a68f345fcda0f599d6e122c7e4161b6e777397e1
JINOK <wlsdhr477@naver.com>
2019-11-22 20:39:09 +0900

    -dictionary 에 대해서 공부하고 dictionary0.py를 추가함1

--- OC_1112/dictionary0.py
+++ OC_1112/dictionary0.py
@@ -1,6 +1,8 @@
 #딕셔너리
 a = {"김도한" : "010-7178-0000"}
 print(a["김도한"])
+a["박지영"]="010-9765-1111"
+print(a)
 
 #리스트
 b = ["010-7178-0000"]
@@ -15,4 +17,12 @@ d={"김도한":"맥주", "이광우":"소주", "정유선":"막걸리"}
 print(d.get("김도한"))
 print(d["김도한"])
 
-#
+#bool type = trud, false
+is_jiyoung_exist = "박지영" in d
+print(type(is_jiyoung_exist))
+print(type(d))
+
+#element 삭제 딕셔너리 - 삭제
+d.pop("김도한")
+del d["이광우"]
+print(d)
